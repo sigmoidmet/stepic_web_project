bf27a20c9006730c4a67a56ffd97153de84c2e8a
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-10-25 20:56:41 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def application(environ, start_response):
     status = '200 OK'
     headers = [
-        ("Content-Type", "text/plain")  
+        ("Content-Type", "text/plain; charset=utf-8")  
         ]
     body = '\n'.join(environ['QUERY_STRING'].split('&')).encode()
     start_response(status, headers)
