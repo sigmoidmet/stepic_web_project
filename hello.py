0c3993bfa02695b30eeb43f2e1332edcc54ff480
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-10-25 20:24:46 +0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+        ]
+    body = '\n'.join(eviron['QUERRY_STRING'].split('&'))
+    start_response(status, headers)
+    return [body]
+  
