a54f34761de480d36b1a66976140c0ef398544aa
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-10-25 20:34:30 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app &
+sudo gunicorn -b 0.0.0.0:8080 /home/box/web/hello.py:app &
