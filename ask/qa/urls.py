519be6e103643b51509d1bba047ed7dc1494bc65
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 16:47:14 +0400

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,1 +1,11 @@
 from qa.views import test
+
+urlpatterns = patterns('qa.views',
+                        url('login/', test, name='login'),
+                        url('signup/', test, name='signup'),
+                        url('question/<int:id>/', test, name='question'),
+                        url('ask/', test, name='ask'),
+                        url('popular/', test, name='popular'),
+                        url('new/', test, name='new'),
+                        url('', test, name='home'),
+                        )
