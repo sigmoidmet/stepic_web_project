3412b7e41fcaec1b83fd05e7b7629be200848a05
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 20:26:49 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo cd ask
-gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
+gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug /home/box/web/ask ask.wsgi:application
