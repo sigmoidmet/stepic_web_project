e1c920fc37a6035954f80ca0b30f682ec92c62ef
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 19:20:05 +0400

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -41,15 +41,16 @@ INSTALLED_APPS = [
     'ask.qa',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+MIDDLEWARE_CLASSES = (                                                          
+    'django.contrib.sessions.middleware.SessionMiddleware',                     
+    'django.middleware.common.CommonMiddleware',                                
+    #'django.middleware.csrf.CsrfViewMiddleware',                               
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',                 
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',          
+    'django.contrib.messages.middleware.MessageMiddleware',                     
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',                  
+    #'django.middleware.security.SecurityMiddleware',                           
+) 
 
 ROOT_URLCONF = 'ask.urls'
 
