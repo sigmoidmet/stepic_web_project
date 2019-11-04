5e4157c5607769fda46cf5a982b9bb0f3721547c
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 19:48:04 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 --pythonpath hello:app &     
-gunicorn -b 0.0.0.0:8000 --pythonpath ask.wsgi:application & 
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web hello:app &     
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
