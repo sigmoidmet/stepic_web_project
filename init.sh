7da0fbc8976e6c3ed78cbc1370975fadb0c8e86b
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 20:19:37 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+virtualenv -p python3 myvenv
+source myvenv/bin/activate
+pip install --upgrade pip
+pip install django
+pip install gunicorn
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo cd ask
 gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
