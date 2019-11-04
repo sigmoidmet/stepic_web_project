2fcf035e7e4e320ca3e97212c513f52e185f78d7
sigmoidmet <49333950+sigmoidmet@users.noreply.github.com>
2019-11-04 20:13:04 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
 from qa.views import test
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('qa.views',                                              
    url(r'^$', 'test'),                                                              
